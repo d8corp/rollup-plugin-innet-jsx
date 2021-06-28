7fb9ade61866e3459e772a3f72ef13976ed6dd3f
Mike Lysikov <d8corp@mail.ru>
2021-06-29 00:35:18 +0300

    release v1.1.2

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,12 @@ export default function jsx () {
       return opt
     },
     transform (code: string, id: string) {
-      return transform(code, {
-        jsxFile: id,
-        parser: code => this.parse(code)
-      })
+      if (/(t|j)sx?$/.test(id)) {
+        return transform(code, {
+          jsxFile: id,
+          parser: code => this.parse(code)
+        })
+      }
     }
   }
 }
